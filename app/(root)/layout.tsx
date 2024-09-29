af51ea76dce96480222bc565032ac372426c3148
ammadkh <ammadhassan104@gmail.com>
2024-09-29 17:52:27 +0500

    add force dynamic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import MobileNavbar from "@/components/MobileNavbar";
 import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.action";
